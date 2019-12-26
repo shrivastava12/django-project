c38f9387efb0c1dd457b38b70fb9276271bd41b4
shrivastava12 <shrivastava747495@gmail.com>
2019-12-26 09:49:24 +0530

    django

---
+++ hindi/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home1(request):
+    return HttpResponse('Nameste in hindi')
+
+
+
---
+++ hola/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepageview(request):
+    return HttpResponse('Hola means hello in spanish')
+
